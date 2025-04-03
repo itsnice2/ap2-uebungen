1d483c4d33299a66220cd54bb86d76dd4a170fa1
itsnice2 <itsnice2@web.de>
2025-04-03 16:05:42 +0200

    20250403-2

--- mvc-pattern/adressencontroller.php
+++ mvc-pattern/adressencontroller.php
@@ -1,1 +1,24 @@
+
+
 <?php 
+
+require_once("adressenmodel.php");
+
+class Adressencontroller{
+
+    public static function adressen_ausgeben(){
+        $adressen = Adressenmodel::zeige_adressen();
+        return $adressen;
+    }
+    
+    public static function adresse_speichern($data){
+        try{
+            Adressenmodel::speicher_adressen($data);
+            return true;
+        }
+        catch(Exception $e){
+            return false;
+        }
+       
+    }
+}
--- mvc-pattern/adressenmodel.php
+++ mvc-pattern/adressenmodel.php
@@ -1,15 +1,22 @@
+
+
 <?php
 
-class Adressenmodel
-{
+class Adressenmodel{
 
-    public function zeige_adressen(){
+    public static function zeige_adressen(){
         $i = 0;
         if (($handle = fopen("adressen.csv", "r")) !== FALSE) {
             while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                $adressen[$i]['nachname'] = $data[0];
-                $adressen[$i]['vorname'] = $data[1];
-                $adressen[$i]['geburtstag'] = $data[2];               
+                $adressen[$i]['id'] = $data[0];
+                $adressen[$i]['nachname'] = $data[1];
+                $adressen[$i]['vorname'] = $data[2];
+                $adressen[$i]['geburtstag'] = $data[3];
+                $adressen[$i]['anschrift'] = $data[4];
+                $adressen[$i]['plz_ort'] = $data[5];
+                $adressen[$i]['telefon'] = $data[6];
+                $adressen[$i]['email'] = $data[7];
+
                 $i++;
             }
             fclose($handle);
@@ -19,16 +26,16 @@ class Adressenmodel
     }
 
 
-    public function speicher_adressen($daten){
-
+    public static function speicher_adressen($daten){
+        #...
     }
 
     public function neue_adressen($daten){
-
+        #...
     }
 
     public function loesche_adressen($id){
-
+        #...
     }
 
 
